ec33e64781a6c136edfa6cb3e3eea04d3c23b433
Prof97 <66251156+Prof97@users.noreply.github.com>
2021-06-29 22:13:49 +0200

    Add files via upload

---
+++ Foodoo1-master/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Foodoo',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
